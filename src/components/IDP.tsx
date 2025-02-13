04fdb509f86e54af4b180ae01566fd5521851bcc
Julianna Lamb <julianna@stytch.com>
2025-02-12 18:40:35 -0800

    fix idp route

--- src/components/IDP.tsx
+++ src/components/IDP.tsx
@@ -1,16 +1,7 @@
-import { useStytchMember, StytchB2BIDP } from "@stytch/react/b2b";
-import { Navigate } from "react-router-dom";
+import { StytchB2BIDP } from "@stytch/react/b2b";
 import { discoveryStyles } from "../utils/stytchConfig";
 
 export const IDP = (): JSX.Element => {
-  const { member } = useStytchMember();
-
-  if (!member) {
-    // Redirect to login, preserving the current URL to return after auth
-    const currentUrl = encodeURIComponent(window.location.href);
-    return <Navigate to={`/?return_to=${currentUrl}`} />;
-  }
-
   return (
     <div className="centered-login">
       <StytchB2BIDP 
